e4cdfde03d00bd2db2f77d1a98596d771bbbcdb0
Vaibhav-1510 <vaibhavarewar@gmail.com>
2024-08-28 13:18:29 +0530

    App file

---
+++ src/EmployeeData.js
@@ -0,0 +1,45 @@
+ export const  EmployeeData= [
+    {
+        id:1,
+        firstName:"John",
+        lastName:"Rogers",
+        age:25
+    },
+    {
+        
+            
+        id:2,
+        firstName:"Tony",
+        lastName:"Stark",
+        age:25
+
+    },
+    {
+        
+        id:3,
+        firstName:"Neeraj",
+        lastName:"Chopra",
+        age:25
+    },
+    {
+        
+        id:4,
+        firstName:"Rohit",
+        lastName:"Sharma",
+        age:32
+    },
+    {
+        
+        id:5,
+        firstName:"Virat",
+        lastName:"Kohli",
+        age:30
+    },
+    {
+        
+        id:6,
+        firstName:"Surya",
+        lastName:"Kumar",
+        age:28
+    }
+]
---
+++ src/Popup.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Popup(){
+
+    return(
+        <div className="h-auto flex flex-col align-items-center gap-6 bg-black text-white">
+                <h1 className="text">Add Details</h1>
+                <button></button>
+        </div>
+    )
+}
+
+export default Popup
